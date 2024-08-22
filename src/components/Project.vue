2f240b4381a5a16ad60ff38161eefbefc0022c30
Khaled Nadam <khalednadam@gmail.com>
2024-08-22 19:08:06 +0300

    fix: :lipstick: fix projects style

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,48 +1,61 @@
 <script setup>
-import { Icon } from '@iconify/vue';
-import { urlFor } from '../plugins/sanity';
+import { Icon } from "@iconify/vue";
+import { urlFor } from "../plugins/sanity";
 
 const props = defineProps({
   img: String,
   title: String,
   link: String,
   technologies: Array,
-  description: String
-})
-
+  description: String,
+});
 </script>
 <template>
   <div v-if="title" class="min-h-full h-full">
-    <v-card variant="outlined" class="min-h-full h-full">
-      <v-card-text>
-        <div class="flex items-start gap-2">
+    <v-card variant="outlined" class="">
+      <v-card-text class="h-full">
+        <div class="flex items-start gap-2 h-max">
           <v-avatar color="white" rounded="lg">
-            <v-img v-if="img" :src="urlFor(img).url()" height="full" min-height="20">
+            <v-img
+              v-if="img"
+              :src="urlFor(img).url()"
+              height="full"
+              min-height="20"
+            >
             </v-img>
           </v-avatar>
           <div>
             <h3>
               {{ title }}
             </h3>
-            <p>
+            <p class="py-2">
               {{ description }}
             </p>
           </div>
         </div>
-        <div class="">
-          <v-chip color="onbackground" variant="tonal" v-for="tech in technologies" class="m-1">
+        <div class="h-max">
+          <v-chip
+            color="onbackground"
+            variant="tonal"
+            v-for="tech in technologies"
+            class="m-1"
+          >
             <Icon :icon="tech" width="17" />
           </v-chip>
         </div>
-        <a :href="link" target="_blank">
-          <v-btn class="w-full mt-2" color="secondary" variant="flat">
-            Visit
-            <Icon icon="ph:arrow-square-out" width="20" class="mx-2" />
-          </v-btn>
-        </a>
+        <div class="mt-5">
+          <a
+            :href="link"
+            target="_blank"
+            class="w-full flex justify-center items-center"
+          >
+            <v-btn class="w-full" color="secondary" variant="flat">
+              Visit
+              <Icon icon="ph:arrow-square-out" width="20" class="mx-2" />
+            </v-btn>
+          </a>
+        </div>
       </v-card-text>
     </v-card>
   </div>
 </template>
-
-
